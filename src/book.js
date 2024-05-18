1c70b978004fa91a813f697a9e2f0de54ab1bc9a
Scott DeVoss <scottdevoss@yahoo.com>
2024-05-18 15:12:23 -0600

    passed 3 more tests in iteration 1

--- src/book.js
+++ src/book.js
@@ -2,10 +2,22 @@ function createTitle(title) {
   return `The ${title}`;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviewArray) {
+  reviewArray.push(review);
+}
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
