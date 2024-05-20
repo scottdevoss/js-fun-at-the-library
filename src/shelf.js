f021a627227aec5f1dfa373a594a66011ff72532
Scott DeVoss <scottdevoss@yahoo.com>
2024-05-20 09:17:56 -0600

    completed iteration 2

--- src/shelf.js
+++ src/shelf.js
@@ -2,8 +2,8 @@
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
 
 function shelfBook(title, shelf) {
@@ -15,5 +15,27 @@ function shelfBook(title, shelf) {
 };
 
 function unshelfBook(title, shelf) {
- 
+  
+  // Find the index of the book to remove
+  const indexToRemove = shelf.findIndex(book => book.title === title);
+
+  // If the book is found, remove it from the shelf
+  if (indexToRemove !== -1) {
+    shelf.splice(indexToRemove, 1);
+  }
+}
+
+function listTitles(shelf) {
+  return shelf.map(function(book) {
+    return book.title;
+  }).join(', ');
+}
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
 }
